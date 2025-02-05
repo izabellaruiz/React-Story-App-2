ca259059bc365823b7e5e79c73935fed7e0af5c0
izabellaruiz <122401811+izabellaruiz@users.noreply.github.com>
2025-02-04 22:13:41 -0500

    Initial commit: Added Cinderella Story App

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main>
+      <p>Cinderella is a fairy tale about a kind young girl who is mistreated by her stepmother and stepsisters...</p>
+      <p>With the help of her Fairy Godmother, she attends a royal ball where she meets the prince...</p>
+      <p>At midnight, she runs away, leaving behind her glass slipper, which helps the prince find her.</p>
+    </main>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>The moral of the story: Kindness and perseverance lead to happiness.</p>
+    </footer>
+  );
+}
+
+export default Footer;
